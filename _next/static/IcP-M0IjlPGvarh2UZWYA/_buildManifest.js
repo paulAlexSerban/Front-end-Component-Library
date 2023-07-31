99e059650c281626f34bdbc3e6dc3a556f1f4764
paulAlexSerban <paul.alex.serban@gmail.com>
2023-07-31 09:56:14 +0000

    Deploying to gh-pages from @ paulAlexSerban/prj--js-component-lib@b27f8df9a5894a30fdace2f6a49549d1d417a596 🚀

---
+++ _next/static/IcP-M0IjlPGvarh2UZWYA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f147620f0ee1ddbb.css","static/chunks/pages/index-d6f6579ebbb58d1c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
